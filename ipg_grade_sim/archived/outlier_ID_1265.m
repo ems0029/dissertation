b0bd8833dd3dd8d4d23eb7a17fd7f0962d348213
Evan Stegner <80865115+ems0029@users.noreply.github.com>
2024-02-22 18:04:22 -0600

    Delete ipg_grade_sim/archived directory

--- ipg_grade_sim/archived/outlier_ID_1265.m
+++
@@ -1,7 +0,0 @@
-
-%% outlier investigation Obs 703
-subtbl = tbl(tbl.ID==stats( ...
-    id(ismembertol(stats.mean_NPC,1.33492,0.0001,'DataScale',1)),:).ID,:);
-
-t1 = subtbl(1:2441,:);
-t2 = setdiff(subtbl,t1)
