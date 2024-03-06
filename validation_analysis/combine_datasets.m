d336670f0a82b72630eaebe4fc002f49f8a3169f
ems0029 <ems0029@auburn.edu>
2024-03-05 18:02:37 -0600

    Update the dataset combinations
    Make function to merge tables

--- validation_analysis/combine_datasets.m
+++ validation_analysis/combine_datasets.m
@@ -4,22 +4,19 @@ pad_adjustment = 'cadj';
 weather = false;
 addpath('.\functions\')
 
-% TODO function form to add a table to another, this is sloppy
-
-load("./lookups/nfc_tbl_aug_canada.mat",'nfc_tbl_aug')
-out = process_nfc_tbl(nfc_tbl_aug,drr_method,pad_adjustment,weather)
-out.dataset = 1*ones(height(out),1);
-load("./lookups/nfc_tbl_aug_jdsmc.mat",'nfc_tbl_aug')
-nfc_tbl_aug.G = nfc_tbl_aug.G+max(out.G)
-nfc_tbl_aug = process_nfc_tbl(nfc_tbl_aug,drr_method,pad_adjustment,weather)
-nfc_tbl_aug.dataset = 1*ones(height(nfc_tbl_aug),1);
-out = out(:,intersect(nfc_tbl_aug.Properties.VariableNames,out.Properties.VariableNames,'stable'))
-
-nfc_tbl_aug = nfc_tbl_aug(:,intersect(nfc_tbl_aug.Properties.VariableNames,out.Properties.VariableNames,'stable'))
-
-nfc_tbl_aug = [out;nfc_tbl_aug]
+% load and merge tables
+table_1 = process_nfc_tbl(load("./lookups/nfc_tbl_aug_jdsmc.mat",'nfc_tbl_aug').nfc_tbl_aug, drr_method, pad_adjustment, weather);
+table_2 = process_nfc_tbl(load("./lookups/nfc_tbl_aug_I85.mat",'nfc_tbl_aug').nfc_tbl_aug, drr_method, pad_adjustment, weather);
+table_3 = process_nfc_tbl(load("./lookups/nfc_tbl_aug_canada.mat",'nfc_tbl_aug').nfc_tbl_aug, drr_method, pad_adjustment, weather);
+% table_4 = process_nfc_tbl(load("./lookups/nfc_tbl_aug_doe.mat",'nfc_tbl_aug').nfc_tbl_aug, drr_method, pad_adjustment, weather);
+nfc_tbl_aug = merge_tables(merge_tables(table_1,table_2),table_3);
 
 scatter3(nfc_tbl_aug.delPAD,nfc_tbl_aug.delPaero,nfc_tbl_aug.delP_true)
 fitlm([nfc_tbl_aug.delPAD,nfc_tbl_aug.delPaero,nfc_tbl_aug.G,nfc_tbl_aug.delP_fan],nfc_tbl_aug.delP_true,'CategoricalVars','x3')
 
-func
+function merged_table = merge_tables(table_1,table_2)
+   %shift groups
+   table_2.G = table_2.G+max(table_1.G);
+   commonvars = intersect(table_1.Properties.VariableNames,table_2.Properties.VariableNames,'stable');
+   merged_table = [table_1(:,commonvars);table_2(:,commonvars)];
+end
